694c63a06e0bdb4fd2e7beb3ee0314352e17b85e
fyang151 <72482284+fyang151@users.noreply.github.com>
2022-03-21 14:49:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function changeColour () {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  const rgb = `rgb(${r},${g},${b})`;
+  document.documentElement.style.setProperty('--acolor', rgb);
+}
+
+window.onload = changeColour
+
+function checkPswd() {
+  var passwordCorrect = "admin";
+  var password = document.getElementById("passwordID").value;
+  if (password == passwordCorrect) {
+    window.location="thing.html";
+  }
+  else if (password == ""){
+    alert("Please Enter a Password")
+  }
+  else{
+    alert("Passwords Dont Match");
+  }
+}
+
+var input = document.getElementById("passwordID");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("plagiarismButton").click();
+  }
+});
